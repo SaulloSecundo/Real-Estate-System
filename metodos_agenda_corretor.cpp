c0578be409eaf95f15ba538d79b22be7532aadde
Yasmin Pinheiro Azevedo <yasminpinheiroy290807y@gmail.com>
2025-07-08 11:45:09 -0300

    third

--- metodos_agenda_corretor.cpp
+++ metodos_agenda_corretor.cpp
@@ -6,3 +6,37 @@
 #include "tipoImovel.h"
 using namespace std;
 
+void cin_Corretor(){
+    int num_corretores;
+    cin >> num_corretores;
+
+    std::vector<Corretor*> corretores;
+
+    for(int ii = 0; ii<num_corretores; ii++){
+        std::string linha;
+        // ler telefone, avaliador, lat, lon
+        string telefone; int avaliador; double lat, lng;
+        cin >> telefone >> avaliador >> lat >> lng;
+        string nome;
+        getline(cin >> std::ws, nome); // lê o restante da linha como nome completo
+
+        corretores.push_back(new Corretor(telefone, avaliador, lat, lng, nome));
+    }
+}
+
+
+void cin_Imovel(){
+    int num_imoveis;
+    cin >> num_imoveis;
+
+    vector<Imovel*> imoveis;
+
+    for(int ii = 0; ii<num_imoveis; ii++){
+        string tipo; int propId; double lat, lng, preco;
+        cin >> tipo >> propId >> lat >> lng >> preco;
+        string endereco;
+        getline(cin >> std::ws, endereco);
+
+        imoveis.push_back(new Imovel(tipo, propId, lat, lng, preco, endereco));
+    }
+}
--- metodos_agenda_corretor.h
+++ metodos_agenda_corretor.h
@@ -1,5 +1,7 @@
 #ifndef _METODOS_AGENDA_CORRETOR_H_
 #define _METODOS_AGENDA_CORRETOR_H_
 
+void cin_corretor();
+void cin_Imovel();
 
 #endif
