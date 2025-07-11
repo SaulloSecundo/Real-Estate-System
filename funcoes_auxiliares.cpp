a868da914d651b2d6a71fbf1c47e94ee4f6a545e
Saulo Carlos <123772993+SaulinhoArrocha@users.noreply.github.com>
2025-07-11 13:56:59 -0300

    Atualização

--- funcoes_auxiliares.cpp
+++ funcoes_auxiliares.cpp
@@ -2,11 +2,15 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <limits>
 #include "funcoes_auxiliares.h"
 #include "corretor.h"
 #include "imovel.h"
 #include "tipoImovel.h"
+
 using namespace std;
+
+constexpr double EARTH_R = 6371.0; // Raio da Terra em km
 
 //método para leitura das entradas referentes aos corretores 
 void lerCorretores(vector<Corretor*>& corretores){
@@ -59,9 +63,6 @@ void distribuirImoveis(vector<vector<Imovel *>> &imoveisDistribuidos, vector<Imo
     }
 }
 
-
-constexpr double EARTH_R = 6371.0; // Raio da Terra em km
-
 double haversine(double lat1, double lon1, double lat2, double lon2) {
     auto deg2rad = [](double d){ return d * M_PI / 180.0; };
     double dlat = deg2rad(lat2 - lat1);
@@ -73,59 +74,62 @@ double haversine(double lat1, double lon1, double lat2, double lon2) {
     return EARTH_R * c;
 }
 
-vector<vector<Imovel* >> agendaVisitas(avaliadores.size());
-for(int ii=0; ii<avaliadores.size(); ii++){
-    for(const auto& c: imoveisDistribuidos){
-        int idImovel = 0;
-        double menorDist = std::numeric_limits<double>::max();
-        for(int jj = 0; jj<c.size(); jj++){
-            if(!c[jj].visitado){
-                double d = haversine(avaliadores[ii].getLat(), avaliadores[ii].getLng(), c[jj].getLat(), c[jj].getLng());
-                if (d < menorDist) {
-                    menorDist = d;
-                    idImovel = jj;
-                }
-                
-            }
-        }
-        for(int jj = 0; jj<c.size(); jj++){
-            if(jj==idImovel){
-                agendaVisitas[ii].push_back(c[jj]);
-            }
-        }
+void hora_minuto(int &horas, int &minutos, int &tempo_acumulado, double distancia){
+    int tempo_chegada = static_cast<int>(distancia * 2);
+    tempo_acumulado = tempo_acumulado + tempo_chegada;
 
-    }
+    horas = (tempo_acumulado - (tempo_acumulado % 60))/60;
+    minutos = tempo_acumulado % 60;
 }
 
-std::vector<std::vector<Imovel*>> agendaVisitas(avaliadores.size());
+//método para estruturar a agenda de visitas de cada corretor
+//Aqui, os imóveis a serem visitados e seus respectivos horários de visita são organizados de forma a tornar eficiente o deslocamento de cada avaliador 
+void definirAgenda(vector<Corretor *> &avaliadores, vector<vector<Imovel *>> &imoveisDistribuidos, vector<vector<Imovel*>> &agendaVisitas, vector<vector<Horario*>> &horarioVisitas){
 
-for (int ii = 0; ii < avaliadores.size(); ii++) {
-    double latAtual = avaliadores[ii].lat;
-    double lngAtual = avaliadores[ii].lng;
+    for (int ii = 0; ii < avaliadores.size(); ii++) {
+        double latAtual = avaliadores[ii]->lat;
+        double lngAtual = avaliadores[ii]->lng;
 
-    // Enquanto houver imóveis atribuídos a este avaliador
-    while (true) {
-        int idImovel = -1;
-        double menorDist = std::numeric_limits<double>::max();
+        int horas = 0;
+        int minutos = 0;
+        int tempo_acumulado = 9*60;
 
-        for (int jj = 0; jj < imoveisDistribuidos[ii].size(); jj++) {
-            Imovel* imv = imoveisDistribuidos[ii][jj];
-            if (!imv->visitado) {
-                double d = haversine(latAtual, lngAtual, imv->getLat(), imv->getLng());
-                if (d < menorDist) {
-                    menorDist = d;
-                    idImovel = jj;
+        // Enquanto houver imóveis atribuídos a este avaliador
+        while (true) {
+            int idImovel = -1;
+
+            //atribui um valor do tipo double inicial à variável de controle da menor distância. 
+            double menorDist = numeric_limits<double>::max();
+
+            //percorre todos os imóveis de um determinado (ii) avaliador
+            //Esse loop foi definido para conseguir a menor distância entre a posição atual do avaliador e um imóvel dentre um subconjuntos de imóveis ainda não visitados
+            //Caso ainda existam imóveis não visitados, a distância é calculada e o id do imóvel mais próximo é armazenado
+            for (int jj = 0; jj < imoveisDistribuidos[ii].size(); jj++) {
+                Imovel* imv = imoveisDistribuidos[ii][jj];
+                if (!imv->visitado) {
+                    double d = haversine(latAtual, lngAtual, imv->getLat(), imv->getLng());
+                    if (d < menorDist) {
+                        menorDist = d;
+                        idImovel = jj;
+                    }
                 }
             }
+
+            //Para controle do loop (while). Se todos os imóveis de um determinado avaliador tiverem sido visitados, idImovel continuará sendo igual a -1 e o loop encerra
+
+            if (idImovel == -1) break; // todos visitados
+
+        
+            Imovel* imovel_a_visitar = imoveisDistribuidos[ii][idImovel];
+            imovel_a_visitar->visitado = true;
+            agendaVisitas[ii].push_back(imovel_a_visitar);
+
+            hora_minuto(horas, minutos, tempo_acumulado, menorDist); //calcula o horário em horas e minutos da visita registrada, dado o tempo acumulado em minutos;
+            horarioVisitas[ii].push_back(new Horario(horas, minutos));
+            tempo_acumulado += 60;
+
+            latAtual = imovel_a_visitar->getLat();  // atualiza posição para o próximo cálculo
+            lngAtual = imovel_a_visitar->getLng();
         }
-
-        if (idImovel == -1) break; // todos visitados
-
-        Imovel* next = imoveisDistribuidos[ii][idImovel];
-        next->visitado = true;
-        agendaVisitas[ii].push_back(next);
-
-        latAtual = next->getLat();  // atualiza posição para o próximo cálculo
-        lngAtual = next->getLng();
     }
 }
--- funcoes_auxiliares.h
+++ funcoes_auxiliares.h
@@ -1,9 +1,18 @@
 #ifndef _FUNCOES_AUXILIARES_H_
 #define _FUNCOES_AUXILIARES_H_
 
+//para definir o horário (em horas e minutos) de cada visita
+struct Horario {
+    int hora;
+    int minuto;
+
+    Horario(int h = 0, int m = 0) : hora(h), minuto(m) {}
+};
+
 void lerCorretores(std::vector<Corretor *> &corretores);
 void lerImoveis(std::vector<Imovel *> &imoveis);
 void corretoresAvaliadores(std::vector<Corretor *> &avaliadores, std::vector<Corretor *> &corretores);
 void distribuirImoveis(std::vector<std::vector<Imovel *>> &imoveisDistribuidos, std::vector<Imovel *> &imoveis, std::vector<Corretor *> &avaliadores);
-
+void hora_minuto(int &horas, int &minutos, double distancia);
+void definirAgenda(std::vector<Corretor *> &avaliadores, std::vector<std::vector<Imovel *>> &imoveisDistribuidos, std::vector<std::vector<Imovel*>> agendaVisitas, std::vector<std::vector<Horario*>> &horarioVisitas);
 #endif
